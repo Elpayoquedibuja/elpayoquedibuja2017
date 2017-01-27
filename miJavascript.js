86e9b9662f24ac4f1ec0ce36f536a5ab3361b956
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-01-27 15:58:55 +0100

    Añadido el botón de ocultar y el javascript para que oculte

--- miJavascript.js
+++ miJavascript.js
@@ -1,15 +1,15 @@
-alert("hola, miJavascript.js");
 function mostrar(){
   var estadoDelCursor = document.getElementById('cartel-jone-info').style.height;
 
-    document.getElementById('cartel-jone-info').style.height = '900px';
+    document.getElementById('cartel-jone-info').style.height = '1100px';
     document.getElementById('ampliar').style.visibility = 'hidden';
 
 }
 
 function ocultar() {
 
-     document.getElementById('oculto').style.height = '10px';
+     document.getElementById('cartel-jone-info').style.height = '10px';
+     document.getElementById('ampliar').style.visibility = 'visible';
 
 }
 
