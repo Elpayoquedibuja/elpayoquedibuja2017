cd31b2d370ff13eb8082282174f0dcc8025a56af
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-01-27 15:33:28 +0100

    Añadido 'miJavascript.js'

---
+++ miJavascript.js
@@ -0,0 +1,18 @@
+function mostrar(){
+  var estadoDelCursor = document.getElementById('oculto').style.height;
+
+    document.getElementById('cartel-jone-info').style.height = '900px';
+
+}
+
+function ocultar() {
+
+     document.getElementById('oculto').style.height = '10px';
+
+}
+
+function ocultarImg() {
+
+     document.getElementById('stone-img').style.left = '420px';
+
+}
