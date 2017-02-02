2d1b5ec84f35b218cffd6b02ee65cb0908217aad
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-02-02 20:16:55 +0100

    El carrousel funciona hacia la derecha

--- miJavascript.js
+++ miJavascript.js
@@ -15,34 +15,66 @@ function ocultar() {
 
 }
 
+function volver_a_empezar(){
+  ocultarImg_dcha();
+}
+
 function ocultarImg_dcha() {
-     document.getElementById('galeria').style.left = '420px';
-     document.getElementById('galeria').style.opacity= 0;
-     document.getElementById('derecha').style.opacity = 0.5;
-     setTimeout(cambioImg_y_la_situo_a_la_izq, 1000);
+      if (num<4) {
+
+      document.getElementById('galeria').style.left = '420px';
+      document.getElementById('galeria').style.opacity= 0;
+      setTimeout(cambioImg_y_la_situo_a_la_izq, 1000);
+      }
+
+
 }
 
 function cambioImg_y_la_situo_a_la_izq() {
-  num++;
-  if(num>3)
-    num=1;
     var cartel = document.getElementById("galeria");
     galeria.src = "img/jone"+num+".png";
     document.getElementById('galeria').style.left = '-420px';
+    num++;
+
     setTimeout(Img_aparece_desde_la_izq, 1000);
+    // alert("¿cuanto vale num? Vale: "+num);
+    // if (num<3) {
+    //   num++;
+    //   setTimeout(Img_aparece_desde_la_izq, 1000);
+    // }
+    //   else {
+    //     alert("vuelvo a empezar");
+    //     num=1;
+    //     alert("vuelvo a empezar, efectivamente: "+num);
+    //     confirmar_dcha();
+    //     }
 }
 
 function Img_aparece_desde_la_izq() {
-  if(num>3)
-    num=1;
+
     var cartel = document.getElementById("galeria");
     galeria.src = "img/jone"+num+".png";
     document.getElementById('galeria').style.opacity = 1;
     document.getElementById('galeria').style.left = '10px';
+    if (num==4) {
+        num=1;
+        document.getElementById('derecha').style.opacity= 0;
+    }
+
+
 
 }
 
+function vuelvo_a_empezar() {
+    console.log("estoy en Img_aparece_desde_la_izq:"+ num);
+    galeria.src = "img/jone"+num+".png";
+    alert("estoy aquí otra vez al principio, no?");
 
+    document.getElementById('galeria').style.opacity = 0.5;
+    document.getElementById('galeria').style.left = '10px';
+
+
+}
 
 function ocultarImg_izda() {
      document.getElementById('jone-img').style.left = '-420px';
