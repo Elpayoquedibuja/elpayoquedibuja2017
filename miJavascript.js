05ba2a1c9a490d4cc83e8fcf8b3bfd6f8d133524
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-02-02 20:31:57 +0100

    Funciona hacia la izquierda, pero con errores

--- miJavascript.js
+++ miJavascript.js
@@ -21,12 +21,18 @@ function volver_a_empezar(){
 
 function ocultarImg_dcha() {
       if (num<4) {
-
       document.getElementById('galeria').style.left = '420px';
       document.getElementById('galeria').style.opacity= 0;
       setTimeout(cambioImg_y_la_situo_a_la_izq, 1000);
       }
+}
 
+function ocultarImg_izda() {
+      if (num<4) {
+      document.getElementById('galeria').style.left = '-420px';
+      document.getElementById('galeria').style.opacity= 0;
+      setTimeout(cambioImg_y_la_situo_a_la_dcha, 1000);
+      }
 
 }
 
@@ -35,19 +41,15 @@ function cambioImg_y_la_situo_a_la_izq() {
     galeria.src = "img/jone"+num+".png";
     document.getElementById('galeria').style.left = '-420px';
     num++;
-
     setTimeout(Img_aparece_desde_la_izq, 1000);
-    // alert("¿cuanto vale num? Vale: "+num);
-    // if (num<3) {
-    //   num++;
-    //   setTimeout(Img_aparece_desde_la_izq, 1000);
-    // }
-    //   else {
-    //     alert("vuelvo a empezar");
-    //     num=1;
-    //     alert("vuelvo a empezar, efectivamente: "+num);
-    //     confirmar_dcha();
-    //     }
+}
+
+function cambioImg_y_la_situo_a_la_dcha() {
+    var cartel = document.getElementById("galeria");
+    galeria.src = "img/jone"+num+".png";
+    document.getElementById('galeria').style.left = '420px';
+    num++;
+    setTimeout(Img_aparece_desde_la_dcha, 1000);
 }
 
 function Img_aparece_desde_la_izq() {
@@ -60,9 +62,18 @@ function Img_aparece_desde_la_izq() {
         num=1;
         document.getElementById('derecha').style.opacity= 0;
     }
+  }
 
+  function Img_aparece_desde_la_dcha() {
 
-
+    var cartel = document.getElementById("galeria");
+    galeria.src = "img/jone"+num+".png";
+    document.getElementById('galeria').style.opacity = 1;
+    document.getElementById('galeria').style.left = '10px';
+    if (num==4) {
+        num=1;
+        document.getElementById('derecha').style.opacity= 0;
+    }
 }
 
 function vuelvo_a_empezar() {
@@ -74,8 +85,4 @@ function vuelvo_a_empezar() {
     document.getElementById('galeria').style.left = '10px';
 
 
-}
-
-function ocultarImg_izda() {
-     document.getElementById('jone-img').style.left = '-420px';
 }
