8d8fc8a8983dde4c3b48e20acd39b06877eae703
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-02-03 12:44:00 +0100

    renombrado 'miJavascrit.js' por 'galeria.js'

--- galeria.js
+++ galeria.js
@@ -1,19 +1,5 @@
 var num = 1;
 
-function mostrar(){
-  var estadoDelCursor = document.getElementById('cartel-jone-info').style.height;
-
-    document.getElementById('cartel-jone-info').style.height = '1100px';
-    document.getElementById('ampliar').style.visibility = 'hidden';
-
-}
-
-function ocultar() {
-
-     document.getElementById('cartel-jone-info').style.height = '10px';
-     document.getElementById('ampliar').style.visibility = 'visible';
-
-}
 
 /* EJEMPLO TAL CUAL */
 // function adelante() {
