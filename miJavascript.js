fef7dba36baf99d1eb26f6d24f8ec5e66215fbeb
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-02-05 15:55:13 +0100

    Añadido id 'ampliar_btn' y :focus outline none

--- miJavascript.js
+++ miJavascript.js
@@ -2,15 +2,25 @@
 function mostrar(){
     var estadoDelCursor = document.getElementById('cartel-jone-info').style.height;
     document.getElementById('cartel-jone-info').style.height = '1100px';
+    document.getElementById('proyecto_con_jone').style.height = '1800px';
     document.getElementById('ampliar').style.visibility = 'hidden';
 
 }
 
 function ocultar() {
 
-     document.getElementById('cartel-jone-info').style.height = '10px';
-     document.getElementById('ampliar').style.visibility = 'visible';
+    document.getElementById('proyecto_con_jone').style.height = '0px';
+    document.getElementById('cartel-jone-info').style.height = '0px';
+    document.getElementById('proyecto_con_jone').style.overflow = 'hidden';
+    document.getElementById('ampliar').style.visibility = 'visible';
+
 
 }
 
 /* Nuevo function mostrar */
+
+function mostrar_proyecto_con_jone(){
+    document.getElementById('proyecto_con_jone').style.height = '800px';
+
+
+}
