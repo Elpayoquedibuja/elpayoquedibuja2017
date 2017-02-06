a872e0edb3169b13a2fc02aadb3dc20ec15b12be
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2017-02-06 12:33:38 +0100

    Nuevos botones ampliar_info-oscuro-01.png y 02.png. Y su programación en miJavascript.js

--- miJavascript.js
+++ miJavascript.js
@@ -1,3 +1,4 @@
+var estado = 1;
 
 function mostrar(){
     var estadoDelCursor = document.getElementById('cartel-jone-info').style.height;
@@ -13,6 +14,9 @@ function ocultar() {
     document.getElementById('cartel-jone-info').style.height = '0px';
     document.getElementById('proyecto_con_jone').style.overflow = 'hidden';
     document.getElementById('ampliar').style.visibility = 'visible';
+    var icono=document.getElementById("ampliar_btn");
+    icono.src = "img/ampliar_info-oscuro-01.png";
+    estado = 1;
 
 
 }
@@ -20,7 +24,19 @@ function ocultar() {
 /* Nuevo function mostrar */
 
 function mostrar_proyecto_con_jone(){
+  if (estado==1) {
     document.getElementById('proyecto_con_jone').style.height = '800px';
-
+    /* Cambiar el icono de la fotografía */
+    var icono=document.getElementById("ampliar_btn");
+    icono.src = "img/ampliar_info-oscuro2-01.png";
+    estado++;
+    }
+    else {
+      document.getElementById('proyecto_con_jone').style.height = '0px';
+      /* Cambiar el icono de la fotografía */
+      var icono=document.getElementById("ampliar_btn");
+      icono.src = "img/ampliar_info-oscuro-01.png";
+      estado = 1;
+    }
 
 }
